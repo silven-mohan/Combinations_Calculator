157a309f61fa8e643fc6be110a5f101b40f317ce
Silven Mohan <silven667@gmail.com>
2025-06-16 22:46:38 +0530

    Basic Combinations Calculator in C using iterative factorials

---
+++ Combinations_Calculator.c
@@ -0,0 +1,26 @@
+/** Combinations Calculator **/
+
+#include<stdlib.h>
+
+main()
+
+{
+	int i, n, r, ncr, nf=1, rf=1, nrf=1;
+	printf("Enter values of n and r:");
+	scanf("%d%d", &n, &r);
+	system("cls");
+	for(i=n;i>=1;i--)
+	{
+		nf=nf*i;
+	}
+	for(i=r;i>=1;i--)
+	{
+		rf=rf*i;
+	}
+	for(i=n-r;i>=1;i--)
+	{
+		nrf=nrf*i;
+	}
+	ncr=nf/(nrf*rf);
+	printf("%dC%d= %d", n, r, ncr);
+}
